3ce4a0a1d2780150fc73a0ba5f3bb4834f1e7744
Regan <reg@cakefarm.com>
2016-07-16 20:37:57 -0700

    ui components

--- imports/ui/layouts/DefaultLayout.jsx
+++ imports/ui/layouts/DefaultLayout.jsx
@@ -3,18 +3,118 @@
  */
 
 import React from 'react'
-//import ExecutionEnvironment from 'react/lib/ExecutionEnvironment';
+//import ExecutionEnvironment from 'react/lib/ExecutionEnvironment'; // TODO: Load differently
 import Navigation from '../components/Navigation';
 import AccountsUIWrapper from '../accounts/accounts_ui_wrapper';
+import LeafletMap from 'leaflet';
 
+
+export const DefaultLayout = React.createClass({
+  /*propTypes: {
+    title: React.PropTypes.string
+  },*/
+  render() {
+    return (
+      <div>
+        <div className="l-header">
+          
+          <Navigation />
+        </div>
+        <div className="l-content">
+          <h1>Other H1 this.props.title </h1>
+          
+
+          this_props_children 
+
+          <div id="map-container"></div>
+
+        </div>
+        <div className="l-footer">
+          <p>Copyright (c) 2014 Company Name</p>
+        </div>
+
+      </div>
+    );
+  }/*,
+  componentDidMount() {
+    if (ExecutionEnvironment.canUseDOM) {
+      //document.title = "this_props_title";
+    }
+  }*/
+});
+
+/* 
+//works
 export const DefaultLayout = (props) => {
  
   return (
   <div>
-   <h1>Our App</h1>
-   <AccountsUIWrapper />
-        <div>{props.content(props)}</div>
-   <Navigation />
+    <div className="l-header">
+      <h1>Our App</h1>
+      <AccountsUIWrapper />
+      <div>{props.content(props)}</div>
+      <Navigation />
+    </div>
+    <div className="l-content">
+      <h1>this.props.title</h1>
+      this.props.children
+    </div>
+    <div className="l-footer">
+      <p>Copyright (c) 2016 Compostable by Team Compost</p>
+    </div>
   </div>
   )
 }
+
+
+
+export const DefaultLayout = (props) => {
+  return (
+  <div>
+    <div className="l-header">
+      <h1>Our App</h1>
+      <AccountsUIWrapper />
+        <div>{props.content(props)}</div>
+      <Navigation />
+    </div>
+    <div className="l-content">
+      <h1>{this.props.title}</h1>
+      {this.props.children}
+    </div>
+    <div className="l-footer">
+      <p>Copyright (c) 2016 Compostable by Team Compost</p>
+    </div>
+  </div>
+  )
+}
+
+
+Example
+export const DefaultLayout = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string
+  },
+  render() {
+    return (
+      <div>
+        <div className="l-header">
+          <h1>Our App</h1>
+          <Navigation />
+        </div>
+        <div className="l-content">
+          <h1>{this.props.title}</h1>
+          {this.props.children}
+        </div>
+        <div className="l-footer">
+          <p>Copyright (c) 2014 Company Name</p>
+        </div>
+      </div>
+    );
+  },
+  componentDidMount() {
+    if (ExecutionEnvironment.canUseDOM) {
+      document.title = this.props.title;
+    }
+  }
+});
+*/
